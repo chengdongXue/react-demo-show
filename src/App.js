8b67c0354a0db6796354fadbec0b7fca2b585bdf
dehua su <dehua.su@hp.com>
2021-07-20 07:03:56 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Communication from "./component/Communication";
+import React, { Component } from "react";
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.backToContractor = this.backToContractor.bind(this);
+    this.state = {
+      employState: 'contractor',
+      staffRemind: 'Become staff whether is sure to succeed? ',
+      salary: 5000,
+      roleMessage: 'Initial message for method of react of componentWillReceiveProps()'
+    }
+  }
+
+  performingStaff() {
+    this.setState({
+      employState: "Staff",
+      salary: 10000,
+      staffRemind: 'You became a staff in HP',
+      roleMessage: 'component of children try to update component of parent'
+    });
+  }
+
+  backToContractor() {
+    const contractor_ = {
+        employState: 'contractor',
+        staffRemind: 'Become staff whether is sure to succeed? ',
+        salary: 4000,
+        roleMessage: 'component of parent try to update component of children'
+    };
+    this.setState(contractor_);
+
+    this.setState(function(state) {
+      console.log(state.salary);
+      return{
+        salary: state.salary + 100
+      }
+    });
+
+    this.setState(function(state) {
+      console.log(state.salary);
+      return{
+        salary: state.salary + 200
+      }
+    });
+
+    this.setState(function(state) {
+      console.log(state.salary);
+      return{
+        salary: state.salary + 300
+      }
+    });
+  }
+
+  render() {
+    // JSX patten codes
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Communication
+            employState={this.state.employState}
+            staffRemind={this.state.staffRemind}
+            salary={this.state.salary}
+            roleMessage={this.state.roleMessage}
+            performingStaff={this.performingStaff.bind(this)}></Communication>
+          <button onClick={this.backToContractor} style={{height: 40, width: 120, color: 'red', fontSize: 20}}>Contractor</button>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/component/Communication.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+
+class Communication extends Component {
+
+    constructor(props){
+        super(props);
+        console.log(`Initial constructor`);
+        this.state = {
+            employees: [{
+                    name: 'Victor',
+                    jobTitle: 'CM',
+                    description: 'I am outsource people.'
+                },{
+                    name: 'Austin',
+                    jobTitle: 'CM',
+                    description: 'I am outsource people too.'
+                }
+            ]
+        }
+        console.log(`Initial state of react`);
+    }
+
+    componentWillMount() {
+        console.log(`componentWillMount(): To executing something before rendering dom of react`);
+    }
+
+    componentWillReceiveProps() {
+        console.log(`componentWillReceiveProps(): It seem sure to call this function when the state or props has update`);
+        // console.log(`componentWillReceiveProps(): It seem sure to call this function when ${this.props.roleMessage}`);
+    }
+
+    shouldComponentUpdate() {
+        console.log(`shouldComponentUpdate(): To determine whether is needs to render at once when it was received new props and state`);
+        // return false is indicate that it was won't render at once
+        // return true is indicate that it seem sure that perform render() function.
+        return true;
+    }
+
+    render() {
+        console.log('render(): rendering page in progress...');
+        return(
+            <div>
+                {
+                    this.state.employees.map((item, index) => {
+                        return (
+                            <ul key={index} style={{fontSize: 14}}>
+                                <li>{item.name}</li>
+                                <li>{item.jobTitle}</li>
+                                <li>{item.description}</li>
+                            </ul>
+                        )
+                    })  
+                }
+                <h3>Employ State Is <span>{this.props.employState}</span></h3>
+                <h3>Your salary is {this.props.salary} RMB</h3>
+                <h3>{this.props.staffRemind}</h3>
+                <h4>Trying click the button as below, Which your mind is being changed by something on future</h4>
+                <button onClick={this.props.performingStaff} style={{height: 40, width: 100, color: 'red', fontSize: 20}}>Staff</button>
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        console.log(`componentDidMount(): To performing something after rendered dom of react`);
+    }
+
+    componentDidUpdate() {
+        console.log(`componentDidUpdate(): The render() function has been rendered, will to call this function`);
+    }
+}
+
+export default Communication;
