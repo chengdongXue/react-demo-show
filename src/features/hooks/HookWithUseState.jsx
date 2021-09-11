da5d0a8346e36b68b0d8b7487dd202ca305edfc3
chengdong-xue <chengdong.xue@hp.com>
2021-09-11 16:17:35 +0800

    piecemeal learning hooks of react

--- src/features/hooks/HookWithUseState.jsx
+++ src/features/hooks/HookWithUseState.jsx
@@ -1,15 +1,70 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 const StudyUseState = () => {
   const [increaseNumber, setIncreaseNumber] = useState(0);
-  const clickNumber = () => {
-    setIncreaseNumber(1);
+  // 不必使用多个state变量,State变量可以很好地存储对象和数组
+  const [selectData, setSelectData] = useState([
+    {
+      id: '1',
+      title: 'China',
+    }, {
+      id: '2',
+      title: 'American',
+    }, {
+      id: '3',
+      title: 'Germany',
+    },
+  ]);
+
+  const useCountries = () => {
+    useEffect(() => {
+      const insertNewData = {
+        id: '4',
+        title: 'Japan',
+      };
+      setSelectData(insertNewData);
+    }, []);
+    return selectData;
   };
+
+  const clickNumber = () => {
+    setIncreaseNumber(increaseNumber + 1);
+  };
+
+  const InvokeEffect = () => {
+    console.log(useCountries());
+  };
+
+  useEffect(() => {
+    document.title = `${increaseNumber}`;
+  }, [increaseNumber]);
+
+  const [height, setHeight] = useState(0);
+
+  const measuredRef = useCallback((node) => {
+    if (node !== null) {
+      setHeight(node.getBoundingClientRect().height);
+    }
+  }, []);
+
   return (
     <div>
-      <h1>Number is </h1>
-      <h2>{increaseNumber}</h2>
-      <button onClick={clickNumber} type="button">Click</button>
+      <div style={{ marginTop: 100 }}>
+        <h1>Number is </h1>
+        <h2>{increaseNumber}</h2>
+        <select>
+          {selectData.map((item) => (
+            <option key={item.id}>{item.title}</option>
+          ))}
+        </select>
+        <button onClick={clickNumber} type="button">Click</button>
+        <button onClick={InvokeEffect} type="button">Invoke custom hook</button>
+      </div>
+      <>
+        <h1 ref={measuredRef}>Hello, world</h1>
+        <h2>The above header is</h2>
+        <h2>{Math.round(height)}</h2>
+      </>
     </div>
   );
 };
